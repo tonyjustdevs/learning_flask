ea080c9728cea88542c68aa6723a6a5af89f9b3c
tonyjustdevs <tonyjustdevs@gmail.com>
2025-03-24 23:59:33 +0700

    add JINJA_NAME param path and default for index.html

--- flask_5/app.py
+++ flask_5/app.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from flask-5!")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def index():
+    url_name = request.args.get("URL_KEY_NAME","MATEYS")
+    return render_template("index.html",JINJA_NAME=url_name)
