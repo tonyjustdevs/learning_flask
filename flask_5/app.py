c9a04c74c642847762b82a86e32836e97d6ed9fc
tonyjustdevs <tonyjustdevs@gmail.com>
2025-03-25 01:21:30 +0700

    add & test input tag outside form tag to index.html - close #11

--- flask_5/app.py
+++ flask_5/app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    url_name = request.args.get("URL_KEY_NAME","MATEYS")
-    return render_template("index.html",JINJA_NAME=url_name)
+    # url_name = request.args.get("URL_KEY_NAME","MATEYS")
+    # return render_template("index.html",JINJA_NAME=url_name)
+    return render_template("index.html")
