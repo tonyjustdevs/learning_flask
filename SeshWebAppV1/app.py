e075a7c3e09bff8188b6c122a2018eae361a6ce1
tonyjustdevs <tonyjustdevs@gmail.com>
2025-04-01 02:39:40 +0700

    add login and logout a-links based on JINJA_NAME existence on index.html - close #77 #78

--- SeshWebAppV1/app.py
+++ SeshWebAppV1/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # name = None
-    name = "messi"
-    return render_template("index.html", JINJA_NAME = name) #NOW add jinja if logic
+    name = "Messi"
+    return render_template("index.html", JINJA_NAME = name) 
+
+ 
+#NOW 
+#78 add logout anchor link to logout route if user is loggedin
+#77 add login anchor link to login route if user logged out to index.html    less than a minute ago
