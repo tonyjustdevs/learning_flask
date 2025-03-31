bf9c167497884e199652760970948e195dc5ba99
tonyjustdevs <tonyjustdevs@gmail.com>
2025-04-01 00:50:26 +0700

    add venv req files layout index app.py for SeshWebAppV1 - initial commit

---
+++ SeshWebAppV1/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template,request, redirect, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = None
+    # name = "messi"
+    return render_template("index.html", JINJA_NAME = name)
