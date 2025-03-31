c9cf8e2f5c5426ef49a47b6f8c0337c76c059ed9
tonyjustdevs <tonyjustdevs@gmail.com>
2025-04-01 02:44:41 +0700

    add logout anchor when user is logged in - close #78

--- SeshWebAppV1/app.py
+++ SeshWebAppV1/app.py
@@ -8,8 +8,3 @@ def index():
     # name = None
     name = "Messi"
     return render_template("index.html", JINJA_NAME = name) 
-
- 
-#NOW 
-#78 add logout anchor link to logout route if user is loggedin
-#77 add login anchor link to login route if user logged out to index.html    less than a minute ago
